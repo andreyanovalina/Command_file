e7811f038d5c08f105bc039ef24b57461279b037
Полина <polina8277@mail.ru>
2019-12-05 15:18:21 +0300

    First commit.

---
+++ file_1.py
@@ -0,0 +1,14 @@
+def my_superfunction():
+    print('What an awesome print!')
+
+
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
+
+
+if __name__ == '__main__':
+    main()
